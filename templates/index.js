604298cdfda666d9ce9b6d4ca48ae543f6977681
Aaron Beavers <foamy125@gmail.com>
2024-12-07 15:05:41 -0600

    updating the config files

--- templates/index.js
+++ templates/index.js
@@ -1,1 +1,29 @@
-create(HtmlElement)("html", {  }, [ create(HtmlElement)("head", {  }, [ create(HtmlElement)("style", {  }, [ "\n"+"        .panel {\n"+"            float: left;\n"+"            border-style: solid;\n"+"            border-width: 1px;\n"+"        }\n"+"        .bordered {\n"+"            border-style: solid;\n"+"            border-width: 1px;\n"+"        }\n"+"\n"+"" ]), create(HtmlElement)("script", { 'src': "https://cdn.jsdelivr.net/quicksettings/3.0/quicksettings.min.js" }, []) ]), "This is a test." ]);
+create(HtmlElement)("html", {  }, [ create(HtmlElement)("head", {  }, [ create(HtmlElement)("style", {  }, [ "\n"+"        .panel {\n"+"            float: left;\n"+"            border-style: solid;\n"+"            border-width: 1px;\n"+"        }\n"+"        .bordered {\n"+"            border-style: solid;\n"+"            border-width: 1px;\n"+"        }\n"+"\n"+"" ]), create(HtmlElement)("script", { 'src': "https://cdn.jsdelivr.net/quicksettings/3.0/quicksettings.min.js" }, []) ]), create(HtmlElement)("script", { 'src': "/socket.io/socket.io.js" }, []), (function() {
+  /* eval.sibilant:19:6 */
+
+  var { 
+    FileSystem
+   } = require("kit-file-system"),
+      Path = require("path");
+  return FileSystem.load("./shared").each(((file) => {
+  	
+    return create(HtmlElement)("script", { 'src': Path.join("/bundles/shared", file.rel) }, []);
+  
+  }));
+}).call(this), (function() {
+  /* eval.sibilant:23:6 */
+
+  var { 
+    FileSystem
+   } = require("kit-file-system"),
+      Path = require("path");
+  return FileSystem.load("./client/obstacles").each(((file) => {
+  	
+    return (function() {
+      if (!(Path.basename(file.path) === "main.js")) {
+        return create(HtmlElement)("script", { 'src': Path.join("/bundles/obstacles", file.rel) }, []);
+      }
+    }).call(this);
+  
+  }));
+}).call(this), create(HtmlElement)("script", { 'src': "/bundles/obstacles/main.js" }, []) ]);
