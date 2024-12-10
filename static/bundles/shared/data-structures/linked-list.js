dc7d7779187de2c2ada08956c9d0e8d8faa5c3c0
Aaron Beavers <foamy125@gmail.com>
2024-12-10 13:11:50 -0600

    Started planning for feature

--- static/bundles/shared/data-structures/linked-list.js
+++ static/bundles/shared/data-structures/linked-list.js
@@ -10729,9 +10729,9 @@ var List = Interface.define("List", {
       var node = this.head;
       var success = false;
       return (function() {
-        var while$5 = undefined;
+        var while$9 = undefined;
         while (node) {
-          while$5 = (function() {
+          while$9 = (function() {
             return (function() {
               if (node.item !== item) {
                 node = node.next;
@@ -10743,7 +10743,7 @@ var List = Interface.define("List", {
             }).call(this);
           }).call(this);
         };
-        return while$5;
+        return while$9;
       }).call(this);
     
    },
@@ -10851,14 +10851,14 @@ var List = Interface.define("List", {
     
       var node = this.head;
       (function() {
-        var while$6 = undefined;
+        var while$10 = undefined;
         while (node) {
-          while$6 = (function() {
+          while$10 = (function() {
             f(node.item, node);
             return node = node.next;
           }).call(this);
         };
-        return while$6;
+        return while$10;
       }).call(this);
       return this;
     
@@ -10868,13 +10868,13 @@ var List = Interface.define("List", {
       var result = create(List)();
       var node = this.head;
       return (function() {
-        var while$7 = undefined;
+        var while$11 = undefined;
         while (node) {
-          while$7 = (function() {
+          while$11 = (function() {
             return result.push(f(node, node.next, node.prev));
           }).call(this);
         };
-        return while$7;
+        return while$11;
       }).call(this);
     
    },
