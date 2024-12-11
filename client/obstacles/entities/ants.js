416cfe1b62326d76ca43de307608b6e8d2bbb983
Err <foamy@gmail.com>
2024-12-11 09:48:17 -0700

    moved some things

--- client/obstacles/entities/ants.js
+++ client/obstacles/entities/ants.js
@@ -1,6 +1,6 @@
 var { 
-  Dot
- } = require("@shared/systems/rendering/dot.js"),
+  AntDots
+ } = require("@obstacles/systems/rendering/ant-dot.js"),
     { 
   Position
  } = require("@obstacles/systems/position.js"),
@@ -27,20 +27,19 @@ var {
   activeGameSystems
  } = require("@obstacles/game.js"),
     config = require("@obstacles/config.js");
-const ants=create(EntityGroup)("Ants", activeGameSystems, game.ent);
+const ants=create(EntityGroup)("Ants", [ Collision, AntDots, Physics, Velocity, Position, AntTrail, AntLifeTimer ], game.ent);
 var clearAnts = (function clearAnts$() {
-  /* clear-ants eval.sibilant:14:0 */
+  /* clear-ants eval.sibilant:15:0 */
 
   return ants.clear();
 });
-var spawnAnt = (function spawnAnt$(x_y$1, home, startingLife) {
-  /* spawn-ant eval.sibilant:15:0 */
+var spawnAnt = (function spawnAnt$(x_y$4, home, startingLife) {
+  /* spawn-ant eval.sibilant:16:0 */
 
-  var x = x_y$1[0],
-      y = x_y$1[1];
+  var x = x_y$4[0],
+      y = x_y$4[1];
 
   var ant = ants.spawn(activeGameSystems);
-  game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
   game.systems.get(Position, ant).x = x;
   game.systems.get(Position, ant).y = y;
   game.systems.get(Position, ant).z = 0;
