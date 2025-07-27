bc85bc63b0cc21c27af109678e55a9487204a453
Error <foamy125@gmail.com>
2025-07-27 11:05:59 -0500

    yo it should be fine...

--- docs/DataStructures/Fields/Sparse/*context.md
+++
@@ -1,9 +0,0 @@
-# DataStructures.Fields.Sparse.*context
-
-## arguments
-
-x y t t* v v* 
-
-## description
-
-If defined, configures variables specific to the given implementation.
--- templates/index.js
+++ templates/index.js
@@ -12,40 +12,57 @@ Object.prototype.each = (function Object$prototype$each$(f) {
   }));
 });
 var navButton = (function navButton$(link, text) {
-  /* nav-button eval.sibilant:19:0 */
+  /* nav-button eval.sibilant:16:0 */
 
   return create(HtmlElement)("html", {  }, [ create(HtmlElement)("span", { 'class': "bordered" }, [ create(HtmlElement)("a", { 'href': link }, [ text ]) ]) ]);
 });
-create(HtmlElement)("html", {  }, [ create(HtmlElement)("head", {  }, [ "\n"+"\n"+"\n"+"       <link rel='preconnect' href='https://fonts.googleapis.com'>\n"+"       <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>\n"+"       <link href='https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap' rel='stylesheet'>\n"+"", create(HtmlElement)("style", {  }, [ "\n"+"* {\n"+"                                       font-family: 'Source Code Pro', serif;\n"+"                                       font-optical-sizing: auto;\n"+"                                       font-weight: 400;\n"+"                                       font-style: normal;\n"+"}\n"+"\n"+"" ]), create(HtmlElement)("html", {  }, [ create(HtmlElement)("style", {  }, [ `
-.panel{
+create(HtmlElement)("html", {  }, [ create(HtmlElement)("head", {  }, [ "\n"+"\n"+"\n"+"       <link rel='preconnect' href='https://fonts.googleapis.com'>\n"+"       <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>\n"+"       <link href='https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap' rel='stylesheet'>\n"+"", create(HtmlElement)("style", {  }, [ "\n"+"* {\n"+"                                       font-family: 'Source Code Pro', serif;\n"+"                                       font-optical-sizing: auto;\n"+"                                       font-weight: 400;\n"+"                                       font-style: normal;\n"+"}\n"+"\n"+"" ]), create(HtmlElement)("style", {  }, [ `
+${".iframe-container"}{
+  height:${"calc(50% - 2px)"};
+  z-index:${"2"};
+  display:${"inline-block"};
+  position:${"relative"};
+  width:${"calc(50% - 4px)"};
+  float:${"left"};
+}
+`, `
+${".iframe-container iframe"}{
+  width:${"calc(100% - 16px)"};
+  height:${"calc(100% - 16px)"};
+  overflow:${"hidden"};
+  pointer-events:${"none"};
+  z-index:${"-100"};
+}
+`, `
+${".panel"}{
   float:${"left"};
   border-style:${"solid"};
   border-width:${"1px"};
 }
 `, `
-.bordered{
+${".bordered"}{
   border-style:${"solid"};
   border-width:${"1px"};
   border-color:${"#75715e"};
 }
 `, `
-.focus-bordered{
+${".focus-bordered"}{
   border-style:${"solid"};
   border-width:${"1px"};
   border-color:${"#99947c"};
 }
 `, `
-body{
+${"body"}{
   background-color:${"#34352f"};
 }
 `, `
-header{
+${"header"}{
   height:${"10%"};
   color:${"#f8f8f2"};
   background-color:${"#1e1f1c"};
 }
 `, `
-main{
+${"main"}{
   height:${"80%"};
   color:${"#f8f8f2"};
   background-color:${"#1e1f1c"};
@@ -53,14 +70,28 @@ main{
   margin-bottom:${"3px"};
 }
 `, `
-footer{
+${"footer"}{
   height:${"10%"};
   color:${"#f8f8f2"};
   background-color:${"brown"};
   background-color:${"#1e1f1c"};
 }
 `, `
-a{
+${"a"}{
   color:${"#cccccc"};
 }
-` ]) ]), create(HtmlElement)("script", { 'src': "https://cdn.jsdelivr.net/quicksettings/3.0/quicksettings.min.js" }, []) ]), create(HtmlElement)("script", { 'src': "/socket.io/socket.io.js" }, []), create(HtmlElement)("body", {  }, [ create(HtmlElement)("header", { 'id': "header" }, [ create(HtmlElement)("h1", {  }, [ "Welcome to the Error Log" ]), create(HtmlElement)("nav", {  }, [ navButton("/projects", "My Projects"), navButton("/prior-work", "My work for others"), navButton("/about-me", "Who am I?"), navButton("/kanban/board.html", "My kanban"), navButton("/docs", "Documentation for Lith"), navButton("/blog", "My Blog") ]) ]), create(HtmlElement)("main", { 'id': "main" }, [ create(HtmlElement)("article", {  }, [ create(HtmlElement)("h2", {  }, [ "I dabble and so can you" ]), create(HtmlElement)("section", {  }, [ "No one should be getting rich. Period.", "Everyone should eat.", "It is now easier than ever to build new and unique software.", "Even grandma can do it." ]), create(HtmlElement)("section", {  }, [ "*INSERT COOL CHAT INTERFACE RUNNING ON YOUR BROWSER TO AN LLM*" ]), create(HtmlElement)("section", {  }, [ "This is your friend. It is free, it is private. If your computer gets hot when you use it, you know why. It's answering your questions with it." ]), create(HtmlElement)("section", {  }, [ "Pretty much everything on this website will do that. Your phone/computer/tablet/toaster is a pretty powerful computer." ]), create(HtmlElement)("section", {  }, [ "And this website will use it. Efficiently. Most graphics are done using low level webgl shaders. Most code heavily optimized. All of it in a language I designed myself (including the templating language I used to make this page) I call 'lith' as in a Lisp with a lisp" ]) ]) ]), create(HtmlElement)("footer", { 'id': "footer" }, [ create(HtmlElement)("section", { 'id': "links" }, [ create(HtmlElement)("a", { 'href': "https://www.linkedin.com/in/aaron-beavers-76b13aa7/" }, [ "linkedin" ]), create(HtmlElement)("a", { 'href': "https://x.com/anomalous_error" }, [ "Twitter/X" ]), create(HtmlElement)("a", { 'href': "https://bsky.app/profile/37707.bsky.social" }, [ "BlueSky" ]), create(HtmlElement)("a", { 'href': "https://github.com/riatzukiza" }, [ "github" ]), create(HtmlElement)("a", { 'href': "https://github.com/riatzukiza/riatzukiza.github.io" }, [ "Source code" ]), create(HtmlElement)("a", { 'href': "https://www.linkedin.com/in/aaron-beavers-76b13aa7/" }, [ "linkedin" ]) ]), create(HtmlElement)("section", { 'id': "contact-info" }, [ create(HtmlElement)("a", { 'href': "mailto:foamy125@gmail.com" }, []) ]) ]) ]) ]);
+` ]), create(HtmlElement)("script", { 'src': "https://cdn.jsdelivr.net/quicksettings/3.0/quicksettings.min.js" }, []) ]), create(HtmlElement)("script", { 'src': "/socket.io/socket.io.js" }, []), create(HtmlElement)("body", {  }, [ create(HtmlElement)("header", { 'id': "header" }, [ create(HtmlElement)("h1", {  }, [ "Welcome to the Error Log" ]), create(HtmlElement)("nav", {  }, [ navButton("/projects", "My Projects"), navButton("/prior-work", "My work for others"), navButton("/about-me", "Who am I?"), navButton("/kanban/board.html", "My kanban"), navButton("/docs", "Documentation for Lith"), navButton("/blog", "My Blog") ]) ]), create(HtmlElement)("main", { 'id': "main" }, [ FileSystem.load("./static/client").find(".").then(((clientDir) => {
+	return clientDir.map(((dir) => {
+	return create(HtmlElement)("a", {
+  'href': ("/projects/" + Path.basename(dir.path) + ".html"),
+  'target': "_blank"
+}, [ create(HtmlElement)("div", { 'class': "iframe-container" }, [ create(HtmlElement)("iframe", {
+  'class': "project-preview",
+  'frameborder': "0",
+  'height': "100%",
+  'width': "100%",
+  'src': ("/projects/" + Path.basename(dir.path) + ".html"),
+  'id': Path.basename(dir.path)
+}, []) ]) ]);
+}));
+})) ]), create(HtmlElement)("footer", { 'id': "footer" }, [ create(HtmlElement)("section", { 'id': "links" }, [ create(HtmlElement)("a", { 'href': "https://www.linkedin.com/in/aaron-beavers-76b13aa7/" }, [ "linkedin" ]), create(HtmlElement)("a", { 'href': "https://x.com/anomalous_error" }, [ "Twitter/X" ]), create(HtmlElement)("a", { 'href': "https://bsky.app/profile/37707.bsky.social" }, [ "BlueSky" ]), create(HtmlElement)("a", { 'href': "https://github.com/riatzukiza" }, [ "github" ]), create(HtmlElement)("a", { 'href': "https://github.com/riatzukiza/riatzukiza.github.io" }, [ "Source code" ]), create(HtmlElement)("a", { 'href': "https://www.linkedin.com/in/aaron-beavers-76b13aa7/" }, [ "linkedin" ]) ]), create(HtmlElement)("section", { 'id': "contact-info" }, [ create(HtmlElement)("a", { 'href': "mailto:foamy125@gmail.com" }, []) ]) ]) ]) ]);
